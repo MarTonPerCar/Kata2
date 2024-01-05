1ed374e615c0d0e80e27ec6fe9ec81c7c35b7d7c
Mario <mario2002pc@hotmail.com>
2024-01-05 14:04:06 +0000

    Final

--- src/main/java/Kata2/Main.java
+++ src/main/java/Kata2/Main.java
@@ -1,4 +1,24 @@
 package Kata2;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    public static void main (String[] args) throws Exception {
+        SalesLoader l = new CsvFileSalesLoader(new File("src/main/10000_Sales_Records.csv"));
+        List<Sales> sales = l.load();
+        Map<String, Integer> Map = new HashMap<>();
+        for (int i = 0; i < sales.size(); i++) {
+            String itemType = sales.get(i).getItem_Type();
+            Map.put(itemType, Map.getOrDefault(itemType, 0) + 1);
+        }
+        for (String k: Map.keySet()) {
+            System.out.println(k + " : " + Map.get(k));
+        }
+
+    }
+
+
 }
